b16f202c4850c07168965f2fd9cf60a020f7a9e9
balinux <ryojuniyantara@gmail.com>
2023-09-20 15:33:40 +0800

    udpate: menambah fungsi save di livewire

--- app/Livewire/Student/Create.php
+++ app/Livewire/Student/Create.php
@@ -2,12 +2,27 @@
 
 namespace App\Livewire\Student;
 
+use App\Models\Classes;
+use App\Models\Student;
 use Livewire\Component;
 
 class Create extends Component
 {
+    public $name, $email, $image, $class_id, $section_id;
+
     public function render()
     {
-        return view('livewire.student.create');
+        return view('livewire.student.create',[
+            // display all classes
+            'classes' => Classes::all()
+        ]);
+    }
+
+    public function save()  {
+        Student::create(
+            $this->only(['name', 'email', 'image', 'class_id', 'section_id'])
+        );
+
+        return redirect('students.index')->with('status','Student has been created');
     }
 }
