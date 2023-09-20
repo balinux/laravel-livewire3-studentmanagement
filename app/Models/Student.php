4f2adeb89f977209613e895040697ab5f71b99aa
balinux <ryojuniyantara@gmail.com>
2023-09-20 14:03:28 +0800

    add: model

---
+++ app/Models/Student.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Spatie\MediaLibrary\HasMedia;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\InteractsWithMedia;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Student extends Model 
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'section_id',
+        'class_id',
+        'name',
+        'email',
+    ];
+
+    public function section()
+    {
+        return $this->belongsTo(Section::class);
+    }
+
+    public function class()
+    {
+        return $this->belongsTo(Classes::class);
+    }
+}
